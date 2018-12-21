0c4f2eb18ef65b57bbe20479d6ab3a17c2cbd2c5
Freddie <fredjlawson@gmail.com>
2018-12-21 17:29:32 +0000

    remove trello params from API call

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,7 +41,7 @@ module.exports = {
     { 
       resolve: 'gatsby-source-apiserver',
       options: {
-        url: `https://api.trello.com/1/lists/598b7836aa7a8937cfd73260/cards?fields=id,name,desc&key=51c8d95afc906bf667ab0535ac782362&token=e0cf35475cd79f9a35d32ff1cc8b1cdf6a006875972e6672a3d395b0da8039e8`,
+        url: `https://api.trello.com/1/lists/598b7836aa7a8937cfd73260/cards?key=51c8d95afc906bf667ab0535ac782362&token=e0cf35475cd79f9a35d32ff1cc8b1cdf6a006875972e6672a3d395b0da8039e8`,
         method: 'get',
         headers: {
           'Content-Type': 'application/json'
