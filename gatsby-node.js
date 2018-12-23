3f97e9e9440ea7105c03c9bbc50d7e458cd039af
Freddie <fredjlawson@gmail.com>
2018-12-23 20:21:12 +0000

    first pass at programatically creating card pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,55 +1,32 @@
-const path = require(`path`)
-const { createFilePath } = require(`gatsby-source-filesystem`)
+const axios = require('axios');
 
+const listId = '598b7836aa7a8937cfd73260'
+const token = 'e0cf35475cd79f9a35d32ff1cc8b1cdf6a006875972e6672a3d395b0da8039e8'
+const key = '51c8d95afc906bf667ab0535ac782362'
+const cardParameters = 'fields=id,name,desc'
+const url = `https://api.trello.com/1/lists/${listId}/cards?${cardParameters}&key=${key}&token=${token}`
 
-exports.onCreateNode = ({ node }) => {
-  console.log(node)
+const getTrelloData = async () => {
+  let response = await axios.get(url) 
+  return response
 }
+
+
+exports.createPages = async ({ actions: { createPage } }) => {
+  const cards = await getTrelloData()
+  cards.forEach(card => {
+    // something to put dashes between the spaces
+    createPage({
+      path: `/${card.name}`,
+      // make generic template component
+      component: require.resolve('./src/templates/blog-post.js'),
+      // don't yet understand what context does
+      context: { card }
+    })
+  });
+}
+
+
 
 // * I want to use the name field to generate the slug.
 // * I think the below adds a field for each node called slug
-
-// exports.onCreateNode = ({ node, getNode, actions }) => {
-//   const { createNodeField } = actions
-//   if (node.internal.type === `MarkdownRemark`) {
-//     const slug = createFilePath({ node, getNode, basePath: `pages` })
-//     createNodeField({
-//       node,
-//       name: `slug`,
-//       value: slug,
-//     })
-//   }
-// }
-
-// exports.createPages = ({ graphql, actions }) => {
-//   const { createPage } = actions
-//   return new Promise((resolve, reject) => {
-//     graphql(`
-//       {
-//         allMarkdownRemark {
-//           edges {
-//             node {
-//               fields {
-//                 slug
-//               }
-//             }
-//           }
-//         }
-//       }
-//     `).then(result => {
-//         result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-//           createPage({
-//             path: node.fields.slug,
-//             component: path.resolve(`./src/templates/blog-post.js`),
-//             context: {
-//               // Data passed to context is available
-//               // in page queries as GraphQL variables.
-//               slug: node.fields.slug,
-//             },
-//           })
-//         })
-      
-//       resolve()
-//     })
-//   })
-// }
