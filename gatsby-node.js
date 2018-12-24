1c58101dcf3ac78f364739f20e5b1db8a4528d08
Freddie <fredjlawson@gmail.com>
2018-12-24 00:05:36 +0000

    successfully programatically generating pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const fetch = require('isomorphic-fetch')
 
 const listId = '598b7836aa7a8937cfd73260'
 const token = 'e0cf35475cd79f9a35d32ff1cc8b1cdf6a006875972e6672a3d395b0da8039e8'
@@ -6,25 +7,29 @@ const key = '51c8d95afc906bf667ab0535ac782362'
 const cardParameters = 'fields=id,name,desc'
 const url = `https://api.trello.com/1/lists/${listId}/cards?${cardParameters}&key=${key}&token=${token}`
 
-const getTrelloData = async () => {
-  let response = await axios.get(url) 
-  return response
-}
 
 
-exports.createPages = async ({ actions: { createPage } }) => {
-  const cards = await getTrelloData()
-  cards.forEach(card => {
-    // something to put dashes between the spaces
-    let slug = card.name.toLowerCase().trim().split(/\s+/).join('-')
-    createPage({
-      path: `/${slug}`,
-      // make generic template component
-      component: require.resolve('./src/templates/blog-post.js'),
-      // don't yet understand what context does
-      context: { card }
-    })
-  });
+
+exports.createPages = ({ actions: { createPage } }) => {
+  
+  fetch(url)
+  .catch(error => console.log('BAD', error))
+  .then(response => {
+    return response.json()
+  })
+  .then(cards => { 
+    cards.forEach(card => {
+        // something to put dashes between the spaces
+      let slug = card.name.toLowerCase().trim().split(/\s+/).join('-')
+      createPage({
+        path: `/${slug}`,
+        // make generic template component
+        component: require.resolve('./src/templates/blog-post.js'),
+        // don't yet understand what context does
+        context: { card }
+      })
+    });
+  })
 }
 
 
