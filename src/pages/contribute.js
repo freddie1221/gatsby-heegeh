ab240039c820a06b530b7e7f7d5e745c164d9c25
Freddie <fredjlawson@gmail.com>
2019-01-18 23:32:08 +0000

    adding state to contribute component

--- src/pages/contribute.js
+++ src/pages/contribute.js
@@ -3,17 +3,41 @@ import Layout from "../components/layout"
 import Textarea from 'react-textarea-autosize'
 
 
-export default () => {
+class Contribute extends React.Component {
 
-  return (
-    <Layout>
-      <h1>Contribute</h1>
-      <p>Add your contribution for review</p>
-      <form>
-        
-        <Textarea></Textarea>
-        
-      </form>
-    </Layout>
-  )
+  constructor(props){
+    super(props)
+    this.state = {title: ""}
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({value: e.target.title});
+  }
+
+  handleSubmit(e) {
+    alert('A name was submitted: ' + this.state.title);
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <Layout>
+        <h1>Contribute</h1>
+        <p>Add your contribution for review{}</p>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value="" onChange={this.handleChange} />
+          <br/><br/>
+          <Textarea 
+            placeholder="Description"
+          /><br/><br/>
+          <input type="submit" value="Submit" />
+          
+        </form>
+      </Layout>
+    )
+  }
 }
+
+export default Contribute
